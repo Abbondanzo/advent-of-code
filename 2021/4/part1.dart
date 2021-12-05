8d0a1d12c0e77e573bd225e2cc39b2c1c612afb0
Abbondanzo <peter@abbondanzo.com>
2021-12-04 22:17:53 -0500

    Part 1, day 4

---
+++ 2021/4/part1.dart
@@ -0,0 +1,130 @@
+import 'package:equatable/equatable.dart';
+import '../utils.dart';
+
+class BingoBoard extends Equatable {
+  // A list of rows of numbers (i.e. index 0 is the top row);
+  final List<List<int>> _numbers;
+  final List<List<bool>> _markedNumbers;
+
+  BingoBoard(this._numbers, this._markedNumbers);
+
+  static BingoBoard fromLines(List<String> lines) {
+    assert(lines.length == 5);
+    final numbers = lines.map((line) {
+      final rowNumbers = line
+          .split(' ')
+          .where((e) => e != '')
+          .map((e) => int.parse(e))
+          .toList();
+      assert(rowNumbers.length == 5);
+      return rowNumbers;
+    }).toList();
+    final markedNumbers = List.filled(5, List.filled(5, false));
+    return BingoBoard(numbers, markedNumbers);
+  }
+
+  bool markNumber(int number) {
+    var causedWin = false;
+    _numbers.asMap().forEach((rowIndex, numberRow) {
+      numberRow.asMap().forEach((colIndex, boardNumber) {
+        if (boardNumber == number) {
+          _markedNumbers[rowIndex][colIndex] = true;
+          causedWin = _causedWin(rowIndex, colIndex);
+        }
+      });
+    });
+    return causedWin;
+  }
+
+  bool _causedWin(int rowIndex, int colIndex) {
+    return _markedNumbers[rowIndex].every((element) => element) ||
+        _markedNumbers.map((row) => row[colIndex]).every((element) => element);
+  }
+
+  int unmarkedNumberSum() {
+    int sum = 0;
+    _markedNumbers.asMap().forEach((rowIndex, markedRow) {
+      markedRow.asMap().forEach((colIndex, marked) {
+        if (!marked) {
+          sum += _numbers[rowIndex][colIndex];
+        }
+      });
+    });
+    return sum;
+  }
+
+  @override
+  List<Object?> get props => [_numbers, _markedNumbers];
+
+  @override
+  String toString() {
+    String output = '';
+    _numbers.asMap().forEach((rowIndex, numberRow) {
+      List<String> rowStr = [];
+      numberRow.asMap().forEach((colIndex, boardNumber) {
+        String numStr = '';
+        if (boardNumber < 10) {
+          numStr += ' ';
+        }
+        numStr += boardNumber.toString();
+        final marked = _markedNumbers[rowIndex][colIndex];
+        if (marked) {
+          numStr += '-';
+        }
+        rowStr.add(numStr);
+      });
+      output += rowStr.join(' ');
+      output += '\n';
+    });
+    return output;
+  }
+}
+
+List<int> parseCalls(List<String> lines) {
+  return lines[0].split(',').map((str) => int.parse(str)).toList();
+}
+
+List<BingoBoard> parseBoards(List<String> lines) {
+  List<BingoBoard> boards = [];
+  List<String> boardLines = [];
+  lines.skip(2).forEach((element) {
+    if (element == '') {
+      assert(boardLines.length == 5);
+      boards.add(BingoBoard.fromLines(boardLines));
+      boardLines = [];
+    } else {
+      boardLines.add(element);
+    }
+  });
+
+  assert(boardLines.length == 5);
+  boards.add(BingoBoard.fromLines(boardLines));
+
+  return boards;
+}
+
+void main() async {
+  final lines = readFile('4/input');
+  final lineList = await lines.toList();
+
+  final calls = parseCalls(lineList);
+  final boards = parseBoards(lineList);
+
+  boards.forEach(print);
+
+  for (var i = 0; i < calls.length; i++) {
+    final call = calls[i];
+    BingoBoard? winner = null;
+    boards.forEach((board) {
+      if (board.markNumber(call)) {
+        winner = board;
+      }
+    });
+    if (winner != null) {
+      final finalScore = winner!.unmarkedNumberSum();
+      print(winner!.toString());
+      print('The final score is ${finalScore * call}');
+      return;
+    }
+  }
+}
