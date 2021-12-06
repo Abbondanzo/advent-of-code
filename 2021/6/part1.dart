aca9123d0099bde0d069b5a4f991339f9b525346
Abbondanzo <peter@abbondanzo.com>
2021-12-06 00:30:54 -0500

    Parts 1 and 2 of day 6

--- 2021/6/part1.dart
+++ 2021/6/part1.dart
@@ -1,6 +1,32 @@
 import '../utils.dart';
 
+List<int> tickFish(List<int> fish) {
+  int numToAdd = 0;
+  final existingFish = fish.map((element) {
+    if (element == 0) {
+      numToAdd++;
+      return 6;
+    } else {
+      return element - 1;
+    }
+  }).toList(); // Must call toList in order to evaluate and have number below
+  return existingFish.followedBy(List.filled(numToAdd, 8)).toList();
+}
+
 void main() async {
   final inputLines = readFile('6/input');
   final inputLineList = await inputLines.toList();
+  assert(inputLineList.length == 1);
+
+  List<int> fish =
+      inputLineList[0].split(',').map((element) => int.parse(element)).toList();
+
+  int day = 0;
+  final totalDays = 80;
+  while (day < totalDays) {
+    day++;
+    fish = tickFish(fish);
+  }
+
+  print('Number of fish after $totalDays days: ${fish.length}');
 }
--- 2021/6/part2.dart
+++ 2021/6/part2.dart
@@ -1,6 +1,43 @@
 import '../utils.dart';
 
-void main() async {
+Future<Map<int, int>> parseInput() async {
   final inputLines = readFile('6/input');
   final inputLineList = await inputLines.toList();
+  assert(inputLineList.length == 1);
+  final fishAgeMap = Map<int, int>();
+  inputLineList[0].split(',').forEach((element) {
+    final age = int.parse(element);
+    fishAgeMap.update(
+      age,
+      (value) => value + 1,
+      ifAbsent: () => 1,
+    );
+  });
+  return fishAgeMap;
+}
+
+Map<int, int> tickFish(Map<int, int> fishAgeMap) {
+  final newFishAgeMap = Map<int, int>();
+  // Create new fish
+  newFishAgeMap[8] = fishAgeMap[0] ?? 0;
+  // Age remaining fish
+  fishAgeMap.forEach((age, count) {
+    final nextAge = age == 0 ? 6 : age - 1;
+    newFishAgeMap.update(nextAge, (value) => value + count,
+        ifAbsent: () => count);
+  });
+  return newFishAgeMap;
+}
+
+void main() async {
+  Map<int, int> fishAgeMap = await parseInput();
+  int day = 0;
+  final totalDays = 256;
+  while (day < totalDays) {
+    day++;
+    fishAgeMap = tickFish(fishAgeMap);
+  }
+  final fishCount =
+      fishAgeMap.values.reduce((value, element) => value + element);
+  print('Number of fish after $totalDays days: $fishCount');
 }
