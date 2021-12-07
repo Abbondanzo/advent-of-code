40f6a0a032ed78993927dd4bacba416204d74077
Abbondanzo <peter@abbondanzo.com>
2021-12-07 00:56:30 -0500

    Day 7

---
+++ 2021/7/part2.dart
@@ -0,0 +1,39 @@
+import '../utils.dart';
+
+Future<List<int>> parseInput() async {
+  // return [16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
+  final inputLines = readFile('7/input');
+  final inputLineList = await inputLines.toList();
+  return inputLineList[0]
+      .split(',')
+      .map((element) => int.parse(element))
+      .toList();
+}
+
+int getFuelCost(List<int> input, int position) {
+  return input.fold(0, (accValue, element) {
+    final distance = (element - position).abs();
+    final elementFuelCost = distance * (distance + 1) ~/ 2;
+    return elementFuelCost + accValue;
+  });
+}
+
+void main() async {
+  final crabPositions = await parseInput();
+  crabPositions.sort((a, b) => a.compareTo(b));
+  final maxPosition = crabPositions[crabPositions.length - 1];
+
+  int? cheapestUsage = null;
+  int cheapestPosition = 0;
+
+  for (int position = 0; position <= maxPosition; position++) {
+    final fuelUsage = getFuelCost(crabPositions, position);
+    if (cheapestUsage == null || fuelUsage < cheapestUsage) {
+      cheapestUsage = fuelUsage;
+      cheapestPosition = position;
+    }
+  }
+
+  // How much fuel must they spend to align to that position?
+  print('Crabs spend $cheapestUsage fuel to reach position $cheapestPosition');
+}
