1f3569cba44d8bb1b6a2db1822f5397cd6cb7562
Abbondanzo <peter@abbondanzo.com>
2021-12-12 00:23:16 -0500

    Part 1

--- 2021/12/part1.dart
+++ 2021/12/part1.dart
@@ -2,11 +2,56 @@ import 'dart:collection';
 
 import '../utils.dart';
 
-Future<void> parseInput() async {
-  final inputLines = readFile('11/input');
+typedef Input = Map<String, List<String>>;
+typedef Path = List<String>;
+
+Future<Input> parseInput() async {
+  final inputLines = readFile('12/input');
   final inputLineList = await inputLines.toList();
+  final Input paths = Map();
+  inputLineList.forEach((line) {
+    final nodes = line.split('-');
+    assert(nodes.length == 2);
+    paths.update(
+      nodes[0],
+      (dests) {
+        dests.add(nodes[1]);
+        return dests;
+      },
+      ifAbsent: () => [nodes[1]],
+    );
+    paths.update(
+      nodes[1],
+      (dests) {
+        dests.add(nodes[0]);
+        return dests;
+      },
+      ifAbsent: () => [nodes[0]],
+    );
+  });
+  return paths;
+}
+
+bool isLowercase(String nodeName) {
+  return nodeName.toLowerCase() == nodeName;
+}
+
+Set<Path> runPath(Input input, Path path) {
+  final curNode = path.last;
+  if (curNode == 'end') return {path};
+  final nextNodes = input[curNode]!.where((nextNode) {
+    return isLowercase(nextNode) ? !path.contains(nextNode) : true;
+  });
+  Set<Path> paths = {};
+  nextNodes.forEach((nextNode) {
+    final nextPath = List<String>.from(path)..add(nextNode);
+    paths.addAll(runPath(input, nextPath));
+  });
+  return paths;
 }
 
 void main() async {
-  final inputLines = await parseInput();
+  final paths = await parseInput();
+  final runPaths = runPath(paths, ['start']);
+  print(runPaths.length);
 }
