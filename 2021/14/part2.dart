c8097e82ce0ba4194269d734162637f1976182ce
Abbondanzo <peter@abbondanzo.com>
2021-12-14 02:18:16 -0500

    Clean up part 2

--- 2021/14/part2.dart
+++ 2021/14/part2.dart
@@ -41,49 +41,54 @@ String expandPolymer(Input input, String startPolymer) {
 }
 
 extension Part2 on Input {
-  String runStepsV2(int numSteps) {
-    String toModify = this.polyTemplate;
+  Map<String, BigInt> runStepsV2(int numSteps) {
+    final bigramCounts = Map<String, BigInt>();
+    final letterCounts = Map<String, BigInt>();
+
+    /// Set initial counts
+    for (int i = 0; i < this.polyTemplate.length - 1; i++) {
+      final bigram = this.polyTemplate.substring(i, i + 2);
+      bigramCounts.update(bigram, (value) => value + BigInt.one,
+          ifAbsent: () => BigInt.one);
+    }
+    for (int i = 0; i < this.polyTemplate.length; i++) {
+      letterCounts.update(this.polyTemplate[i], (value) => value + BigInt.one,
+          ifAbsent: () => BigInt.one);
+    }
+
     List.generate(numSteps, (index) {
-      print('Step ${index + 1}/$numSteps (len ${toModify.length})');
-      toModify = expandPolymer(this, toModify);
+      bigramCounts.entries.toList().forEach((entry) {
+        final insertionChar = this.insertionRules[entry.key]!;
+        final left = entry.key[0] + insertionChar;
+        final right = insertionChar + entry.key[1];
+        bigramCounts.update(entry.key, (value) => value - entry.value);
+        bigramCounts.update(left, (value) => value + entry.value,
+            ifAbsent: () => entry.value);
+        bigramCounts.update(right, (value) => value + entry.value,
+            ifAbsent: () => entry.value);
+        letterCounts.update(insertionChar, (value) => value + entry.value,
+            ifAbsent: () => entry.value);
+      });
     });
-    return toModify;
+
+    return letterCounts;
   }
 }
 
-// String runStep(Input input, String curString) {
-//   List<String> foo = ['N', 'N', 'N', 'C', 'C', 'B'];
-//   foo.s
-
-//   String output = '';
-//   while (output.length < curString.length * 2 - 1) {
-//     final remaining = curString.substring(output.length);
-//     final hasRule = additionalInsertionRules.entries.firstWhereOrNull((element) => curString.startsWith(element.key));
-//     if (hasRule != null) {
-//       output += additionalInsertionRules[hasRule.key]!;
-//     } else {
-//       output += input.insertionRules[]
-//     }
-//   }
-//   if (hasRule) {
-
-//   }
-//   if (curString.st)
-// }
-
 void main() async {
-  final input = await parseInput('14/demo');
+  final input = await parseInput('14/input');
 
-  final outputStr = input.runStepsV2(40);
-  // final nextOutput = expandPolymer(input, outputStr);
+  final letterCounts = input.runStepsV2(40);
+  BigInt? lowest;
+  BigInt? highest;
+  letterCounts.values.forEach((value) {
+    if (lowest == null || value < lowest!) {
+      lowest = value;
+    }
+    if (highest == null || value > highest!) {
+      highest = value;
+    }
+  });
 
-  // print(nextOutput.length);
-  // print(input.runSteps(11).length);
-
-  // if (outputStr != input.runSteps(11)) {
-  //   throw new Error('Fuck');
-  // }
-
-  // final output = mostCommonMinusLeastCommon(outputStr);
-  // print(output);
+  print(highest! - lowest!);
 }
