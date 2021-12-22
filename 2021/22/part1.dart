b79ec5ae2137f143fbbfc857ed3174f3b05c092a
Abbondanzo <peter@abbondanzo.com>
2021-12-22 00:51:43 -0500

    Part 1, day 22

--- 2021/22/part1.dart
+++ 2021/22/part1.dart
@@ -1,6 +1,25 @@
+import '../utils.dart';
 import './parse.dart';
 
 void main() async {
-  final input = await parseInput('22/demo');
-  print(input.initializationProcedure);
+  final input = await parseInput('22/input');
+
+  final naiveMap = Map<String, bool>();
+  input.initializationProcedure.forEach((step) {
+    for (int x in range(step.coords[0], step.coords[1] + 1)) {
+      for (int y in range(step.coords[2], step.coords[3] + 1)) {
+        for (int z in range(step.coords[4], step.coords[5] + 1)) {
+          final key = '($x,$y,$z)';
+          if (step.turnOn) {
+            naiveMap[key] = true;
+          } else {
+            naiveMap.remove(key);
+          }
+        }
+      }
+    }
+  });
+
+  /// Afterward, considering only cubes in the region x=-50..50,y=-50..50,z=-50..50, how many cubes are on?
+  print(naiveMap.length);
 }
