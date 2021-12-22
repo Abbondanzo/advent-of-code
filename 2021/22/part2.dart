c20ca5d3b48a9cc15c4e2ef6e9b1581a52413c6b
Abbondanzo <peter@abbondanzo.com>
2021-12-22 01:43:08 -0500

    Working part 2

--- 2021/22/part2.dart
+++ 2021/22/part2.dart
@@ -19,28 +19,123 @@ class Group {
         zStart <= other.zEnd &&
         other.zStart <= zEnd;
   }
+
+  List<Group> carve(Group other) {
+    List<Group> subgroups = [];
+
+    // Carve from -X
+    int newXStart = xStart;
+    if (xStart < other.xStart) {
+      newXStart = other.xStart;
+      final negXGroup =
+          Group(xStart, other.xStart - 1, yStart, yEnd, zStart, zEnd);
+      subgroups.add(negXGroup);
+    }
+
+    // Carve from +X
+    int newXEnd = xEnd;
+    if (xEnd > other.xEnd) {
+      newXEnd = other.xEnd;
+      final posXGroup = Group(other.xEnd + 1, xEnd, yStart, yEnd, zStart, zEnd);
+      subgroups.add(posXGroup);
+    }
+
+    // Carve from -Y
+    int newYStart = yStart;
+    if (yStart < other.yStart) {
+      newYStart = other.yStart;
+      final negYGroup =
+          Group(newXStart, newXEnd, yStart, other.yStart - 1, zStart, zEnd);
+      subgroups.add(negYGroup);
+    }
+
+    // Carve from +Y
+    int newYEnd = yEnd;
+    if (yEnd > other.yEnd) {
+      newYEnd = other.yEnd;
+      final posYGroup =
+          Group(newXStart, newXEnd, other.yEnd + 1, yEnd, zStart, zEnd);
+      subgroups.add(posYGroup);
+    }
+
+    // Carve from -Z
+    if (zStart < other.zStart) {
+      final negZGroup = Group(
+          newXStart, newXEnd, newYStart, newYEnd, zStart, other.zStart - 1);
+      subgroups.add(negZGroup);
+    }
+
+    // Carve from +Z
+    if (zEnd > other.zEnd) {
+      final posZGroup =
+          Group(newXStart, newXEnd, newYStart, newYEnd, other.zEnd + 1, zEnd);
+      subgroups.add(posZGroup);
+    }
+
+    return subgroups;
+  }
+
+  int get numCubes {
+    return (xEnd - xStart + 1) * (yEnd - yStart + 1) * (zEnd - zStart + 1);
+  }
+
+  @override
+  String toString() {
+    return 'Group($xStart..$xEnd,$yStart..$yEnd,$zStart..$zEnd)';
+  }
 }
 
 void main() async {
-  final input = await parseInput('22/demo2');
+  final input = await parseInput('22/input');
+
+  // final groupA = Group(0, 3, 0, 3, 0, 3);
+  // final groupB = Group(1, 2, 1, 2, 1, 2);
+  // print(groupA.carve(groupB).map((e) => e.numCubes));
+  // print(groupA.numCubes);
+  // print(groupB.numCubes);
+  // print(groupB.carve(groupA));
+  // final groupC = Group(2, 4, 2, 4, 2, 4);
 
   final List<Group> groupList = [];
-  input.initializationProcedure.forEach((step) {
+
+  void handleStep(Step step) {
     final group = Group(step.coords[0], step.coords[1], step.coords[2],
         step.coords[3], step.coords[4], step.coords[5]);
     if (step.turnOn) {
       groupList.add(group);
     } else {
-      print(groupList.where((element) => element.intersects(group)).length);
+      final intersectGroups =
+          groupList.where((onGroup) => onGroup.intersects(group)).toList();
+      intersectGroups.forEach((onGroup) {
+        groupList.remove(onGroup);
+        groupList.addAll(onGroup.carve(group));
+      });
     }
-  });
-  input.outsideProcedure.forEach((step) {
-    final group = Group(step.coords[0], step.coords[1], step.coords[2],
-        step.coords[3], step.coords[4], step.coords[5]);
-    if (step.turnOn) {
-      groupList.add(group);
-    } else {
-      print(groupList.where((element) => element.intersects(group)).length);
+  }
+
+  input.initializationProcedure.forEach(handleStep);
+  input.outsideProcedure.forEach(handleStep);
+
+  int getTotalCubes() {
+    int output = 0;
+    final copiedList = List.from(groupList);
+
+    while (copiedList.isNotEmpty) {
+      final Group currentGroup = copiedList.removeAt(0);
+      output += currentGroup.numCubes;
+
+      final intersectGroups = copiedList
+          .where((otherGroup) => otherGroup.intersects(currentGroup))
+          .toList();
+      intersectGroups.forEach((otherGroup) {
+        copiedList.remove(otherGroup);
+        copiedList.addAll(otherGroup.carve(currentGroup));
+      });
     }
-  });
+
+    return output;
+  }
+
+  final finalCubeCount = getTotalCubes();
+  print(finalCubeCount);
 }
