8d1fdad3e3b93c298781d2c746a70d80d6d5620f
Abbondanzo <peter@abbondanzo.com>
2022-12-03 00:18:51 -0500

    Day 03

--- 2022/03/part1.js
+++ 2022/03/part1.js
@@ -1,2 +1,24 @@
 const fs = require("fs");
 const data = fs.readFileSync("input").toString();
+
+const priorityVal = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let total = 0;
+data
+  .split("\n")
+  .filter(Boolean)
+  .forEach((line) => {
+    const mid = line.length / 2;
+    const start = line.slice(0, mid);
+    const end = line.slice(mid);
+    for (let i = 0; i < end.length; i++) {
+      const char = end[i];
+      if (start.includes(char)) {
+        total += 1 + priorityVal.indexOf(char);
+        break;
+      }
+    }
+  });
+
+// What is the sum of the priorities of those item types?
+console.log(total);
--- 2022/03/part2.js
+++ 2022/03/part2.js
@@ -1,2 +1,21 @@
 const fs = require("fs");
 const data = fs.readFileSync("input").toString();
+const lines = data.split("\n").filter(Boolean);
+
+const priorityVal = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let total = 0;
+
+for (let i = 0; i < lines.length; i += 3) {
+  const [a, b, c] = lines.slice(i, i + 3);
+  for (let j = 0; j < c.length; j++) {
+    const char = c[j];
+    if (a.includes(char) && b.includes(char)) {
+      total += priorityVal.indexOf(char) + 1;
+      break;
+    }
+  }
+}
+
+// What is the sum of the priorities of those item types?
+console.log(total);
