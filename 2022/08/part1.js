0e9cd23f31adce14fbf60d0d1d3d4e8c4c9158b9
Abbondanzo <peter@abbondanzo.com>
2022-12-08 00:49:26 -0500

    Day 08

--- 2022/08/part1.js
+++ 2022/08/part1.js
@@ -1,3 +1,77 @@
 const fs = require("fs");
 const data = fs.readFileSync("input").toString();
 const lines = data.split("\n").filter(Boolean);
+
+const trees = lines.map((row) => row.trim().split("").map(Number));
+
+const outsideVisible = trees.length * 2 + trees[0].length * 2;
+
+let insideVisible = new Set();
+
+for (let rowIdx = 1; rowIdx < trees.length - 2; rowIdx++) {
+  const row = trees[rowIdx];
+
+  // Scan left to right
+  let minHeight = row[0];
+  for (let colIdx = 1; colIdx < row.length - 2; colIdx++) {
+    const tree = row[colIdx];
+    if (tree > minHeight) {
+      if (rowIdx === 1 && colIdx === 65) {
+        throw new Error("stop");
+      }
+      insideVisible.add(`${rowIdx}-${colIdx}-${tree}`);
+      minHeight = tree;
+    }
+  }
+
+  // Scan right to left
+  const maxHeight = minHeight;
+  minHeight = row[row.length - 1];
+  for (let colIdx = row.length - 2; colIdx > 0; colIdx--) {
+    const tree = row[colIdx];
+    if (tree > minHeight) {
+      if (rowIdx === 1 && colIdx === 65) {
+        throw new Error("stop");
+      }
+      insideVisible.add(`${rowIdx}-${colIdx}-${tree}`);
+      minHeight = tree;
+    }
+    // Avoid looping over the rest
+    if (tree === maxHeight) {
+      break;
+    }
+  }
+}
+
+for (let colIdx = 1; colIdx < trees[0].length - 2; colIdx++) {
+  // Scan top to bottom
+  let minHeight = trees[0][colIdx];
+  for (let rowIdx = 1; rowIdx < trees.length - 2; rowIdx++) {
+    const tree = trees[rowIdx][colIdx];
+    if (tree > minHeight) {
+      insideVisible.add(`${rowIdx}-${colIdx}-${tree}`);
+      minHeight = tree;
+    }
+  }
+
+  // Scan bottom to top
+  const maxHeight = minHeight;
+  minHeight = trees[trees.length - 1][colIdx];
+  for (let rowIdx = trees.length - 2; rowIdx > 0; rowIdx--) {
+    const tree = trees[rowIdx][colIdx];
+    if (tree > minHeight) {
+      if (rowIdx === 1 && colIdx === 65) {
+        throw new Error("stop");
+      }
+      insideVisible.add(`${rowIdx}-${colIdx}-${tree}`);
+      minHeight = tree;
+    }
+    // Avoid looping over the rest
+    if (tree === maxHeight) {
+      break;
+    }
+  }
+}
+
+// console.log(insideVisible);
+console.log(outsideVisible + insideVisible.size);
--- 2022/08/part2.js
+++ 2022/08/part2.js
@@ -1,3 +1,61 @@
 const fs = require("fs");
 const data = fs.readFileSync("input").toString();
 const lines = data.split("\n").filter(Boolean);
+const trees = lines.map((row) => row.trim().split("").map(Number));
+
+const evaluateScenicScore = (rowIdx, colIdx) => {
+  let r;
+  let c;
+  const height = trees[rowIdx][colIdx];
+
+  if (rowIdx === 0 || rowIdx === trees.length - 1) {
+    return 0;
+  }
+
+  if (colIdx === 0 || colIdx === trees[0].length - 1) {
+    return 0;
+  }
+
+  // Look left
+  c = colIdx - 1;
+  while (c > 0 && trees[rowIdx][c] < height) {
+    c--;
+  }
+  const left = colIdx - c;
+
+  // Look right
+  c = colIdx + 1;
+  while (c < trees[0].length - 1 && trees[rowIdx][c] < height) {
+    c++;
+  }
+  const right = c - colIdx;
+
+  // Look up
+  r = rowIdx - 1;
+  while (r > 0 && trees[r][colIdx] < height) {
+    if (r === 0) {
+      break;
+    }
+    r--;
+  }
+  const up = rowIdx - r;
+
+  // Look down
+  r = rowIdx + 1;
+  while (r < trees.length - 1 && trees[r][colIdx] < height) {
+    r++;
+  }
+  const down = r - rowIdx;
+
+  return left * right * up * down;
+};
+
+let max = 0;
+for (let rowIdx = 0; rowIdx < trees.length; rowIdx++) {
+  const row = trees[rowIdx];
+  for (let colIdx = 0; colIdx < row.length; colIdx++) {
+    max = Math.max(max, evaluateScenicScore(rowIdx, colIdx));
+  }
+}
+
+console.log(max);
