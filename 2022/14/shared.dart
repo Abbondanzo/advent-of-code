74d291cf57bb360daca76df925d4c2505364985b
Abbondanzo <peter@abbondanzo.com>
2022-12-13 00:34:17 -0500

    Prep day 14

---
+++ 2022/14/shared.dart
@@ -0,0 +1,8 @@
+import '../utils.dart';
+
+Future<List<String>> parseInput(String path) async {
+  final inputLines = readFile(path);
+  final inputLineList = await inputLines.toList();
+
+  return inputLineList;
+}
