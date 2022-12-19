17f48e205d3fa65ea5f66f0231b48c1992fb48dd
Abbondanzo <peter@abbondanzo.com>
2022-12-19 01:41:31 -0500

    Save progress

--- 2022/19/part1.dart
+++ 2022/19/part1.dart
@@ -1,6 +1,134 @@
-import './shared.dart';
+import 'package:equatable/equatable.dart';
+
 import '../utils.dart';
+import './shared.dart';
+
+int MAX_TIME = 24;
+
+class GameState {
+  int _oreRobots = 1;
+  int _clayRobots = 0;
+  int _obsidianRobots = 0;
+  int _geodeRobots = 0;
+
+  int _ore = 0;
+  int _clay = 0;
+  int _obsidian = 0;
+  int _geodes = 0;
+
+  final Blueprint _blueprint;
+
+  GameState(this._blueprint);
+
+  void simulate() {
+    _ore += _oreRobots;
+    _clay += _clayRobots;
+    _obsidian += _obsidianRobots;
+    _geodes += _geodeRobots;
+  }
+
+  GameState copy() {
+    final newState = GameState(_blueprint);
+    newState._ore = _ore;
+    newState._clay = _clay;
+    newState._obsidian = _obsidian;
+    newState._geodes = _geodes;
+    newState._oreRobots = _oreRobots;
+    newState._clayRobots = _clayRobots;
+    newState._obsidianRobots = _obsidianRobots;
+    newState._geodeRobots = _geodeRobots;
+    return newState;
+  }
+
+  bool shouldSkip() {
+    return (_ore > _blueprint.oreRobot) &&
+        (_ore > _blueprint.clayRobot) &&
+        (_ore > _blueprint.obsidianRobot.first &&
+            (_clay > _blueprint.obsidianRobot.second || _clayRobots == 0)) &&
+        (_ore > _blueprint.geodeRobot.first &&
+            (_obsidian > _blueprint.geodeRobot.second || _obsidianRobots == 0));
+  }
+
+  List<GameState> buildOptions() {
+    List<GameState> options = [];
+    if (_ore >= _blueprint.oreRobot) {
+      final newState = copy();
+      newState._ore -= _blueprint.oreRobot;
+      newState.simulate();
+      newState._oreRobots++;
+      options.add(newState);
+    }
+    if (_ore >= _blueprint.clayRobot) {
+      final newState = copy();
+      newState._ore -= _blueprint.clayRobot;
+      newState.simulate();
+      newState._clayRobots++;
+      options.add(newState);
+    }
+    if (_ore >= _blueprint.obsidianRobot.first &&
+        _clay >= _blueprint.obsidianRobot.second) {
+      final newState = copy();
+      newState._ore -= _blueprint.obsidianRobot.first;
+      newState._clay -= _blueprint.obsidianRobot.second;
+      newState.simulate();
+      newState._obsidianRobots++;
+      options.add(newState);
+    }
+    if (_ore >= _blueprint.geodeRobot.first &&
+        _obsidian >= _blueprint.geodeRobot.second) {
+      final newState = copy();
+      newState._ore -= _blueprint.geodeRobot.first;
+      newState._obsidian -= _blueprint.geodeRobot.second;
+      newState.simulate();
+      newState._geodeRobots++;
+      options.add(newState);
+    }
+    return options;
+  }
+
+  @override
+  String toString() {
+    return "GS($_ore+$_oreRobots, $_clay+$_clayRobots, $_obsidian+$_obsidianRobots, $_geodes+$_geodeRobots)";
+  }
+}
+
+int evaluateState(int timeElapsed, GameState state, Map<String, int> seen) {
+  final stateKey = state.toString();
+  // if (timeElapsed == MAX_TIME) {
+  //   print(stateKey);
+  // }
+  if (seen.containsKey(stateKey)) return seen[stateKey]!;
+  if (timeElapsed >= MAX_TIME) return state._geodes;
+  final options = state.buildOptions();
+  final newState = state.copy();
+  newState.simulate();
+  int maxGeodes = newState.shouldSkip()
+      ? 0
+      : evaluateState(timeElapsed + 1, newState, seen);
+  for (final nextState in options) {
+    final totalGeodes = evaluateState(timeElapsed + 1, nextState, seen);
+    if (totalGeodes > maxGeodes) {
+      maxGeodes = totalGeodes;
+    }
+  }
+  seen[stateKey] = maxGeodes;
+  return maxGeodes;
+}
+
+int evaluateBlueprint(Blueprint blueprint) {
+  GameState state = GameState(blueprint);
+  return evaluateState(0, state, {});
+}
 
 void main() async {
-  final input = await parseInput('19/demo');
+  final input = await parseInput('19/input');
+  int total = 0;
+  for (int idx in range(input.length)) {
+    final blueprint = input[idx];
+    final totalGeodes = evaluateBlueprint(blueprint);
+    total += totalGeodes * (idx + 1);
+    print("${idx + 1}/${input.length} (+$totalGeodes)");
+  }
+  // TODO: >1640
+  print(total);
 }
--- 2022/19/shared.dart
+++ 2022/19/shared.dart
@@ -1,9 +1,56 @@
 import '../utils.dart';
 
-typedef Input = List<String>;
+class Blueprint {
+  // ore
+  final int oreRobot;
+  // ore
+  final int clayRobot;
+  // ore/clay
+  final Pair<int, int> obsidianRobot;
+  // ore/obsidian
+  final Pair<int, int> geodeRobot;
+
+  Blueprint(this.oreRobot, this.clayRobot, this.obsidianRobot, this.geodeRobot);
+
+  @override
+  String toString() {
+    return "Blueprint($oreRobot, $clayRobot, $obsidianRobot, $geodeRobot)";
+  }
+}
+
+typedef Input = List<Blueprint>;
 
 Future<Input> parseInput(String path) async {
   final inputLines = readFile(path);
   final inputLineList = await inputLines.toList();
-  return inputLineList;
+  final oreRobotRegExp = new RegExp(r"Each ore robot costs (\d+) ore");
+  final clayRobotRegExp = new RegExp(r"Each clay robot costs (\d+) ore");
+  final obsidianRobotRegExp =
+      new RegExp(r"Each obsidian robot costs (\d+) ore and (\d+) clay");
+  final geodeRobotRegExp =
+      new RegExp(r"Each geode robot costs (\d+) ore and (\d+) obsidian");
+  List<int> parseInts(RegExp expr, String line) {
+    final match = expr.firstMatch(line)!;
+    List<int> ints = [];
+    for (int group in range(1, match.groupCount + 1)) {
+      ints.add(int.parse(match.group(group)!));
+    }
+    return ints;
+  }
+
+  return inputLineList.map((line) {
+    final oreRobotRaw = parseInts(oreRobotRegExp, line);
+    assert(oreRobotRaw.length == 1);
+    final clayRobotRaw = parseInts(clayRobotRegExp, line);
+    assert(clayRobotRaw.length == 1);
+    final obsidianRobotRaw = parseInts(obsidianRobotRegExp, line);
+    assert(obsidianRobotRaw.length == 2);
+    final geodeRobotRaw = parseInts(geodeRobotRegExp, line);
+    assert(geodeRobotRaw.length == 2);
+    return Blueprint(
+        oreRobotRaw[0],
+        clayRobotRaw[0],
+        Pair(obsidianRobotRaw[0], obsidianRobotRaw[1]),
+        Pair(geodeRobotRaw[0], geodeRobotRaw[1]));
+  }).toList();
 }
