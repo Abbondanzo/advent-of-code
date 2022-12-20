479b1674bb7294ab4af798a5e4663ef4a9b44398
Abbondanzo <peter@abbondanzo.com>
2022-12-20 01:39:28 -0500

    Prep day 21

---
+++ 2022/21/part2.dart
@@ -0,0 +1,6 @@
+import '../utils.dart';
+import './shared.dart';
+
+void main() async {
+  final input = await parseInput('21/demo');
+}
---
+++ 2022/21/shared.dart
@@ -0,0 +1,11 @@
+import '../utils.dart';
+
+typedef Input = List<int>;
+
+Future<Input> parseInput(String path) async {
+  final inputLines = readFile(path);
+  final inputLineList = await inputLines.toList();
+  return inputLineList.map((line) {
+    return int.parse(line);
+  }).toList();
+}
