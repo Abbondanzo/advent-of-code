bf91df8ef0cc34bac5b08484e4d791ddbe8775e4
Abbondanzo <peter@abbondanzo.com>
2023-12-04 00:29:14 -0500

    Day 4

--- 2023/src/main/kotlin/04.kt
+++ 2023/src/main/kotlin/04.kt
@@ -1,15 +1,40 @@
 import Utils.readFileAsList
+import kotlin.math.pow
 
-private fun partOne(input: List<String>) {
-    TODO("Not yet implemented")
+val DIGIT_REGEX = Regex("\\d+")
+
+private fun getWins(row: String): Int {
+    val split = row.split(":")[1].split("|")
+    assert(split.size == 2)
+    val winningNumbers = DIGIT_REGEX.findAll(split[0]).map { matchResult -> matchResult.value.toInt() }.toSet()
+    val yourNumbers = DIGIT_REGEX.findAll(split[1]).map { matchResult -> matchResult.value.toInt() }.toList()
+    return yourNumbers.count { number -> winningNumbers.contains(number) }
 }
 
-private fun partTwo(input: List<String>) {
-    TODO("Not yet implemented")
+private fun partOne(input: List<String>): Int {
+    var total = 0
+    input.map { card ->
+        val matches = getWins(card)
+        total += 2.0.pow(matches - 1).toInt()
+    }
+    return total
+}
+
+private fun partTwo(input: List<String>): Int {
+    val winMap = input.map { card -> getWins(card) }.toMutableList()
+    val cards = winMap.map { 1 }.toMutableList()
+    cards[0] = 1
+    for (index in winMap.indices) {
+        val winCount = winMap[index]
+        for (i in (index + 1)..index + winCount) {
+            cards[i] = cards[i] + cards[index]
+        }
+    }
+    return cards.reduce { acc, i -> acc + i }
 }
 
 fun main() {
-    val input = readFileAsList("04/demo").filter(String::isNotEmpty).map(String::trim)
+    val input = readFileAsList("04/input").filter(String::isNotEmpty).map(String::trim)
     println("Part 1: ${partOne(input)}")
-//    println("Part 2: ${partTwo(input)}")
+    println("Part 2: ${partTwo(input)}")
 }
