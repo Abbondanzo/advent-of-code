31c6cf11c1b234bc1fcf93c218080bef285639c7
Abbondanzo <peter@abbondanzo.com>
2023-12-06 00:27:43 -0500

    Prep day 7 and 8 for template

---
+++ 2023/src/main/kotlin/08.kt
@@ -0,0 +1,15 @@
+import Utils.readFileAsList
+
+private fun partOne(input: List<String>) {
+    TODO("Not yet implemented")
+}
+
+private fun partTwo(input: List<String>) {
+    TODO("Not yet implemented")
+}
+
+fun main() {
+    val input = readFileAsList("08/demo").map(String::trim).filter(String::isNotEmpty)
+    println("Part 1: ${partOne(input)}")
+//    println("Part 2: ${partTwo(input)}")
+}
