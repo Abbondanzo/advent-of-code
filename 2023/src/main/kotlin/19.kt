c78d41a36c1c6398c81dd167eb07ac8ab4e81397
Abbondanzo <peter@abbondanzo.com>
2023-12-18 04:03:06 -0500

    Prep days 19 and 20

---
+++ 2023/src/main/kotlin/19.kt
@@ -0,0 +1,17 @@
+import Utils.readFileAsList
+
+private object Day19 {
+  fun partOne(input: List<String>) {
+    TODO("Not yet implemented")
+  }
+
+  fun partTwo(input: List<String>) {
+    TODO("Not yet implemented")
+  }
+}
+
+fun main() {
+  val input = readFileAsList("19/demo").map(String::trim).filter(String::isNotEmpty)
+  println("Part 1: ${Day19.partOne(input)}")
+  //    println("Part 2: ${partTwo(input)}")
+}
---
+++ 2023/src/main/kotlin/20.kt
@@ -0,0 +1,17 @@
+import Utils.readFileAsList
+
+private object Day20 {
+  fun partOne(input: List<String>) {
+    TODO("Not yet implemented")
+  }
+
+  fun partTwo(input: List<String>) {
+    TODO("Not yet implemented")
+  }
+}
+
+fun main() {
+  val input = readFileAsList("20/demo").map(String::trim).filter(String::isNotEmpty)
+  println("Part 1: ${Day20.partOne(input)}")
+  //    println("Part 2: ${partTwo(input)}")
+}
