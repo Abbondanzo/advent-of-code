f1eeb6a2e9aa396c0587be88189286f8a9dd239a
Abbondanzo <peter@abbondanzo.com>
2023-12-20 03:07:43 -0500

    Finish day 20

--- 2023/src/main/kotlin/20.kt
+++ 2023/src/main/kotlin/20.kt
@@ -19,6 +19,18 @@ private object Day20 {
 
     fun getNextPulse(): Pulse?
 
+    data class Empty(override val name: String) : Module {
+      override val outputs: List<String> = listOf()
+
+      override fun handlePulse(module: Module, pulse: Pulse) {
+        // No-op
+      }
+
+      override fun getNextPulse(): Pulse? {
+        return null
+      }
+    }
+
     data object Button : Module {
       override val name: String = "button"
       override val outputs: List<String> = listOf("broadcaster")
@@ -30,7 +42,6 @@ private object Day20 {
       override fun getNextPulse(): Pulse {
         return Pulse.LOW
       }
-
     }
 
     data class Broadcaster(override val outputs: List<String>) : Module {
@@ -70,14 +81,15 @@ private object Day20 {
 
     data class Conjunction(override val name: String, override val outputs: List<String>) : Module {
       private var receivedInputs = mutableMapOf<String, Pulse>()
+      var onPulse: (Module, Pulse) -> Unit = { _, _ -> }
 
       override fun handlePulse(module: Module, pulse: Pulse) {
+        onPulse(module, pulse)
         this.receivedInputs[module.name] = pulse
       }
 
       override fun getNextPulse(): Pulse {
         val allHighPulses = receivedInputs.all { it.value == Pulse.HIGH }
-        receivedInputs = mutableMapOf()
         return if (allHighPulses) Pulse.LOW else Pulse.HIGH
       }
     }
@@ -98,12 +110,26 @@ private object Day20 {
           }
       mutable[module.name] = module
     }
+    // Initialize all conjunction modules
+    for (entry in mutable) {
+      entry.value.outputs.forEach { moduleName ->
+        val nextModule = mutable[moduleName] ?: return@forEach
+        if (nextModule is Module.Conjunction) {
+          nextModule.handlePulse(entry.value, Pulse.LOW)
+        }
+      }
+    }
     return mutable
   }
 
   data class Event(val sender: Module, val receiver: Module, val pulse: Pulse) {
     override fun toString(): String {
       return "${sender.name} -${pulse.name.lowercase(Locale.getDefault())}-> ${receiver.name}"
+    }
+
+    fun process(): Pulse? {
+      receiver.handlePulse(sender, pulse)
+      return receiver.getNextPulse()
     }
   }
 
@@ -117,10 +143,9 @@ private object Day20 {
         Pulse.LOW -> lowCount++
         Pulse.HIGH -> highCount++
       }
-      event.receiver.handlePulse(event.sender, event.pulse)
-      val nextPulse = event.receiver.getNextPulse() ?: continue
+      val nextPulse = event.process() ?: continue
       for (nextModuleName in event.receiver.outputs) {
-        val nextModule = input[nextModuleName] ?: continue
+        val nextModule = input[nextModuleName] ?: Module.Empty(nextModuleName)
         val nextEvent = Event(event.receiver, nextModule, nextPulse)
         queue.add(nextEvent)
       }
@@ -132,7 +157,6 @@ private object Day20 {
     var lowCount = 0L
     var highCount = 0L
     for (i in 1..1000) {
-      println(i)
       val result = runCycle(input)
       lowCount += result.first
       highCount += result.second
@@ -140,13 +164,39 @@ private object Day20 {
     return lowCount * highCount
   }
 
-  fun partTwo(input: List<String>) {
-    //    TODO("Not yet implemented")
+  private fun lcm(a: Long, b: Long): Long {
+    val larger = if (a > b) a else b
+    val max = a * b
+    var lcm = larger
+    while (lcm <= max) {
+      if (lcm % a == 0L && lcm % b == 0L) {
+        return lcm
+      }
+      lcm += larger
+    }
+    return max
+  }
+
+  fun partTwo(input: Map<String, Module>): Long {
+    val predecessor = input.values.find { it.outputs.contains("rx") && it.outputs.size == 1 } ?: error("No way to rx")
+    val requiredFromModuleCount = input.values.count { it.outputs.contains(predecessor.name) && it.outputs.size == 1 }
+    val trackedCycles = mutableMapOf<String, Long>()
+    var presses = 0L
+    (predecessor as Module.Conjunction).onPulse = { from, pulse ->
+      if (pulse == Pulse.HIGH) {
+        trackedCycles[from.name] = presses
+      }
+    }
+    while (trackedCycles.size < requiredFromModuleCount) {
+      presses++
+      runCycle(input)
+    }
+    return trackedCycles.values.reduce { acc, l -> lcm(acc, l) }
   }
 }
 
 fun main() {
   val input = readFileAsList("20/input").map(String::trim).filter(String::isNotEmpty)
   println("Part 1: ${Day20.partOne(Day20.toModules(input))}")
-  println("Part 2: ${Day20.partTwo(input)}")
+  println("Part 2: ${Day20.partTwo(Day20.toModules(input))}")
 }
