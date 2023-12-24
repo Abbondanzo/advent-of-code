11c0784ebb2a8b289c35b04a208d5dec6d0f7128
Abbondanzo <peter@abbondanzo.com>
2023-12-23 19:31:08 -0500

    Save demo days

--- 2023/src/main/kotlin/23.kt
+++ 2023/src/main/kotlin/23.kt
@@ -1,8 +1,50 @@
 import Utils.readFileAsList
+import java.util.*
 
 private object Day23 {
+
+  data class Coordinate(val x: Int, val y: Int) {
+
+    private fun isWall(input: List<String>, coordinate: Coordinate): Boolean {
+      return input[coordinate.y][coordinate.x] == '#'
+    }
+
+    fun getNeighbors(input: List<String>): List<Coordinate> {
+      val maybeNeighbors =
+          when (input[y][x]) {
+            '>' -> listOf(Coordinate(x + 1, y))
+            'v' -> listOf(Coordinate(x, y + 1))
+            '<' -> listOf(Coordinate(x - 1, y))
+            '^' -> listOf(Coordinate(x, y - 1))
+            '.' ->
+                listOf(
+                    Coordinate(x + 1, y),
+                    Coordinate(x - 1, y),
+                    Coordinate(x, y - 1),
+                    Coordinate(x, y + 1))
+            else -> error("Unrecognized character ${input[y][x]}")
+          }
+      return maybeNeighbors.filter { !isWall(input, it) }
+    }
+  }
+
+  private fun fatDFS(start: Coordinate, end: Coordinate, input: List<String>) {
+    val visited = mutableSetOf<Coordinate>()
+    val stack = mutableListOf(start)
+    //    val parents = mutableMapOf<Coordinate, Coordinate>
+    //    while (stack.isNotEmpty()) {
+    //
+    //    }
+  }
+
   fun partOne(input: List<String>) {
-    // TODO("Not yet implemented")
+    val start = Coordinate(0, 1)
+    val visited = mutableSetOf<Coordinate>(start)
+    val queue = LinkedList<Coordinate>()
+    queue.add(start)
+    //    while (queue.isNotEmpty()) {
+    //
+    //    }
   }
 
   fun partTwo(input: List<String>) {
