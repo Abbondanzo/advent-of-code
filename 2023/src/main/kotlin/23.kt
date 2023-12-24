5ddaa25611f0fd8b31f221cc01403e27e8409d85
Abbondanzo <peter@abbondanzo.com>
2023-12-24 10:26:10 -0500

    Part 1

--- 2023/src/main/kotlin/23.kt
+++ 2023/src/main/kotlin/23.kt
@@ -1,4 +1,5 @@
 import Utils.readFileAsList
+import java.lang.Integer.max
 import java.util.*
 
 private object Day23 {
@@ -9,46 +10,109 @@ private object Day23 {
       return input[coordinate.y][coordinate.x] == '#'
     }
 
-    fun getNeighbors(input: List<String>): List<Coordinate> {
-      val maybeNeighbors =
-          when (input[y][x]) {
-            '>' -> listOf(Coordinate(x + 1, y))
-            'v' -> listOf(Coordinate(x, y + 1))
-            '<' -> listOf(Coordinate(x - 1, y))
-            '^' -> listOf(Coordinate(x, y - 1))
-            '.' ->
-                listOf(
-                    Coordinate(x + 1, y),
-                    Coordinate(x - 1, y),
-                    Coordinate(x, y - 1),
-                    Coordinate(x, y + 1))
-            else -> error("Unrecognized character ${input[y][x]}")
-          }
-      return maybeNeighbors.filter { !isWall(input, it) }
+    fun getNeighbors(input: List<String>, ignoreSlopes: Boolean): List<Coordinate> {
+      val rawChar = input[y][x]
+      if (rawChar !in ">v<^.") {
+        error("Unrecognized character ${input[y][x]} $y $x")
+      }
+      val maybeNeighbors = if (ignoreSlopes) {
+        listOf(
+          Coordinate(x + 1, y),
+          Coordinate(x - 1, y),
+          Coordinate(x, y - 1),
+          Coordinate(x, y + 1)
+        )
+      } else {
+        when (rawChar) {
+          '>' -> listOf(Coordinate(x + 1, y))
+          'v' -> listOf(Coordinate(x, y + 1))
+          '<' -> listOf(Coordinate(x - 1, y))
+          '^' -> listOf(Coordinate(x, y - 1))
+          '.' ->
+            listOf(
+              Coordinate(x + 1, y),
+              Coordinate(x - 1, y),
+              Coordinate(x, y - 1),
+              Coordinate(x, y + 1)
+            )
+
+          else -> error("Unrecognized character ${input[y][x]} $y $x")
+        }
+      }
+      return maybeNeighbors.filter { it.x in input[0].indices && it.y in input.indices && !isWall(input, it) }
     }
   }
 
-  private fun fatDFS(start: Coordinate, end: Coordinate, input: List<String>) {
-    val visited = mutableSetOf<Coordinate>()
-    val stack = mutableListOf(start)
-    //    val parents = mutableMapOf<Coordinate, Coordinate>
-    //    while (stack.isNotEmpty()) {
-    //
-    //    }
-  }
+  private fun findLongestPath(input: List<String>, ignoreSlopes: Boolean): Int {
+    val start = Coordinate(1, 0)
+    val end = Coordinate(input[0].length - 2, input.size - 1)
+    val parents = mutableMapOf<Coordinate, Pair<Coordinate, Int>>()
+    parents[start] = Coordinate(0, 0) to 0
+    fun containsCycle(node: Coordinate, neighbor: Coordinate): Boolean {
+      var cur = parents[node]?.first
+      while (cur != null) {
+        if (cur == neighbor) {
+          return true
+        }
+        cur = parents[cur]?.first
+      }
+      return false
+    }
 
-  fun partOne(input: List<String>) {
-    val start = Coordinate(0, 1)
-    val visited = mutableSetOf<Coordinate>(start)
     val queue = LinkedList<Coordinate>()
     queue.add(start)
-    //    while (queue.isNotEmpty()) {
-    //
-    //    }
+    while (queue.isNotEmpty()) {
+      val node = queue.poll()
+      val distance = parents[node]?.second ?: 0
+      for (neighbor in node.getNeighbors(input, ignoreSlopes)) {
+        val maybeParent = parents[neighbor]
+        if (maybeParent == null || (maybeParent.second <= distance && !containsCycle(node, neighbor))) {
+          parents[neighbor] = node to distance + 1
+          queue.add(neighbor)
+        }
+      }
+    }
+
+//    val toPrint = input.toMutableList()
+//    var cur: Coordinate? = end
+//    var count = 0
+//    while (cur != null) {
+//      val coordinate = cur!!
+//      toPrint[coordinate.y] = toPrint[coordinate.y].replaceRange(coordinate.x, coordinate.x + 1, "${count % 10}")
+//      cur = parents[cur]?.first
+//      count++
+//    }
+//    for (line in toPrint) {
+//      println(line)
+//    }
+
+    return parents[end]!!.second
   }
 
-  fun partTwo(input: List<String>) {
-    // TODO("Not yet implemented")
+  private fun findLongestPathV2(
+    input: List<String>,
+    visited: Set<Coordinate>,
+    current: Coordinate,
+    distance: Int,
+  ): Int {
+    val end = Coordinate(input[0].length - 2, input.size - 1)
+    if (current == end) {
+      return distance
+    }
+    var longestPath = Int.MIN_VALUE
+    val newVisited = visited + current
+    for (neighbor in current.getNeighbors(input, true)) {
+      longestPath = max(longestPath, findLongestPathV2(input, newVisited, neighbor, distance + 1))
+    }
+    return longestPath
+  }
+
+  fun partOne(input: List<String>): Int {
+    return findLongestPath(input, false)
+  }
+
+  fun partTwo(input: List<String>): Int {
+    return findLongestPath(input, true)
   }
 }
 
