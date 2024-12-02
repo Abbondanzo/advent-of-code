d4544aaa93f0a6612bed2b6a81343295ae912353
Abbondanzo <peter@abbondanzo.com>
2024-12-01 21:02:32 -0500

    Setup for first 10 days

---
+++ 2024/src/main/kotlin/10.kt
@@ -0,0 +1,16 @@
+import Utils.readFileAsList
+
+private fun partOne(lines: List<String>): Int {
+  return 0
+}
+
+
+private fun partTwo(lines: List<String>): Int {
+  return 0
+}
+
+fun main() {
+  val input = readFileAsList("10/demo").filter { it.isNotEmpty() }
+  println("Part 1: ${partOne(input)}")
+  println("Part 2: ${partTwo(input)}")
+}
