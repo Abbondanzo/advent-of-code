f44ff2c31ba9f8587b17a06ae72a7b12e732a767
Peter Abbondanzo <abbo@meta.com>
2024-12-25 19:21:28 -0600

    Working day 19

--- 2024/src/main/kotlin/19.kt
+++ 2024/src/main/kotlin/19.kt
@@ -1,15 +1,42 @@
 import Utils.readFileAsList
 
-private fun partOne(lines: List<String>): Int {
-  return 0
+private fun getPossibleCombinations(options: List<String>, display: String, memory: MutableMap<String, Long>): Long {
+  return memory.getOrPut(display) {
+    options.fold(0L) { count, option ->
+      when {
+        display == option -> count + 1
+        display.startsWith(option) -> count + getPossibleCombinations(options, display.removePrefix(option), memory)
+        else -> count
+      }
+    }
+  }
 }
 
-private fun partTwo(lines: List<String>): Int {
-  return 0
+private fun partOne(options: List<String>, displays: List<String>): Int {
+  var total = 0
+  val memory = mutableMapOf<String, Long>()
+  for (display in displays) {
+    val combos = getPossibleCombinations(options, display, memory)
+    if (combos > 0) {
+      total++
+    }
+  }
+  return total
+}
+
+private fun partTwo(options: List<String>, displays: List<String>): Long {
+  var total = 0L
+  val memory = mutableMapOf<String, Long>()
+  for (display in displays) {
+    total += getPossibleCombinations(options, display, memory)
+  }
+  return total
 }
 
 fun main() {
-  val input = readFileAsList("19/demo").filter { it.isNotEmpty() }
-  println("Part 1: ${partOne(input)}")
-  println("Part 2: ${partTwo(input)}")
+  val input = readFileAsList("19/input")
+  val options = input[0].split(",").map(String::trim).sortedBy { it.length }
+  val displays = input.subList(2, input.size).filter(String::isNotEmpty)
+  println("Part 1: ${partOne(options, displays)}")
+  println("Part 2: ${partTwo(options, displays)}")
 }
